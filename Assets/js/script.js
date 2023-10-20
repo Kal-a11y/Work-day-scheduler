df875d1e319257ce68a34ea244c016a176253fde
Kaila <kailaamia@gmail.com>
2023-10-20 17:13:47 -0500

    Added pseudo code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -62,6 +62,18 @@ $(function () {
   // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
   //HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  //look at 9am line
+  //when it is (9:00) at 0 seconds do this...
+    //if 9 am is the current time
+      //remove past and future
+      //add present
+    //if 9 am is before current time (less)
+      //remove future and present
+      //add past
+    //if 9am is after current time (more)
+      //remove present and past
+      //add future
 });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
