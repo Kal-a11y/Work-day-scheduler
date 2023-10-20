327689cf5aacbf6aca6465ea9294af0b4d2d1229
Kaila <kailaamia@gmail.com>
2023-10-20 14:36:49 -0500

    Added pseudo code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -54,6 +54,9 @@ $(function () {
   createTimeBlockElements();
 
   // TODO: Add code to display the current date in the header of the page. id = currentDay
+    //get element location
+    //text is currentDay = dayjs().
+    //update every second
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -68,15 +71,10 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
 
